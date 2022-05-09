45c45fb5ef6c41f713a7ef8866fb09dc6e666ec3
Marcus Pará <marcus.para@elo7.com>
2022-05-09 11:48:14 -0300

    changing message interface to abstract class

--- src/models/message/Message.java
+++ src/models/message/Message.java
@@ -1,23 +1,76 @@
 package models.message;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 
 import models.user.User;
 
-public interface Message {
+public abstract class Message {
+    private Integer id;
+    private static int latestId = 0;
+    private String content;
+    private Date sendDate;
+    private User sentFrom;
+    private Collection<User> viewedBy;
 
-    public Integer getId();
+    protected Message(User sentFrom) {
+        this.id = getNextId();
+        this.sendDate = new Date();
+        this.viewedBy = new ArrayList<>();
+        this.sentFrom = sentFrom;
+    }
 
-    public Date getSendDate();
-    public void setSendDate(Date date);
+    public Message(User sentFrom, String content) {
+        this.id = getNextId();
+        this.sendDate = new Date();
+        this.content = content;
+        this.viewedBy = new ArrayList<>();
+        this.sentFrom = sentFrom;
+    }
 
-    public User getSentFrom();
-    public void setSentFrom(User user);
+    private static int getNextId() {
+        return ++latestId;
+    }
 
-    public Collection<User> getViewedBy();
-    public void setViewedBy(Collection<User> viewedBy);
-    public void addViewedBy(User user);
-    
-    public String getContent();
+    public Integer getId() {
+        return this.id;
+    }
+
+    public Date getSendDate() {
+        return sendDate;
+    }
+
+    public void setSendDate(Date sendDate) {
+        this.sendDate = sendDate;
+    }
+
+    public User getSentFrom() {
+        return sentFrom;
+    }
+
+    public void setSentFrom(User sentFrom) {
+        this.sentFrom = sentFrom;
+    }
+
+    public Collection<User> getViewedBy() {
+        return viewedBy;
+    }
+
+    public void setViewedBy(Collection<User> viewedBy) {
+        this.viewedBy = viewedBy;
+        
+    }
+
+    public void addViewedBy(User user) {
+        this.viewedBy.add(user);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
